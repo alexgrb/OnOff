08a911dfb9334fe929a55bb12ea6d43e269723af
pth <piranavanpth@gmail.com>
2020-04-15 08:02:57 +0200

    Added a second platform with restrictions

--- simple canvas/control.js
+++ simple canvas/control.js
@@ -1,70 +1,71 @@
-//defining variables, drawing context, controller, hero and the loop
-var context, controller, hero;
+/********************************************************
+Variables : defining variables, drawing context, controller, hero and the loop
+ ********************************************************/
+var context, controller, monster;
 var score = 0;
 var lives = 3;
-const timeElem = document.querySelector("#time");
-var requestId;
-context = document.querySelector("canvas").getContext("2d");
 
+/********************************************************
+ Setup the canvas
+ ********************************************************/
+let canvas = document.getElementById("game");
+context = document.querySelector("canvas").getContext("2d");
 context.canvas.height = 500;
 context.canvas.width = 500;
 
+/********************************************************
+ Elements appearing on the screen
+ ********************************************************/
 var sun = {
     x: 34,
     y: 122
 };
-
-var star = {
-    x: 450,
-    y: 350
-};
-
-hero = {
-
-    height: 32,
-    //is it in the air?
-    jumping: true,
-    width: 32,
-    x: 250, // center of the canvas
-    y: 0,
-    //speed the rectyngle is going
-    x_velocity: 0,
-    y_velocity: 0
-
-};
-
 var sunImage = new Image();
 sunImage.src = "images/sunvf.png";
 
-var monsterImage = new Image();
-monsterImage.src = "images/monster.png";
-
+var star = {
+    x: 300,
+    y: 300
+};
 var starImage = new Image();
 starImage.src = "images/starvf.png";
+
+monster = {
+    height: 32,
+    jumping: true, //is it in the air?
+    width: 32,
+    x: 50, // left on the canvas
+    y: 0,
+    x_velocity: 0, //speed the avatar
+    y_velocity: 0
+};
+
+var monsterImage = new Image();
+monsterImage.src = "images/monster.png";
 
 controller = {
     left: false,
     right: false,
     up: false,
     keyListener: function (event) {
-        var key_state = (event.type == "keydown") ? true : false;
-        switch (event.keyCode) {
-            case 37:// left key
+        let key_state = (event.type == "keydown") ? true : false;
+        switch (event.code) {
+            case "ArrowLeft":
                 controller.left = key_state;
-                //hero.x -= hero.speed * event;
                 break;
-            case 38:// up key
+            case "ArrowUp":
                 controller.up = key_state;
-                //hero.y -= hero.speed * event;
                 break;
-            case 39:// right key
+            case "ArrowRight":
                 controller.right = key_state;
-                //hero.x += hero.speed * event;
                 break;
         }
     }
 };
 
+/********************************************************
+ Drawings
+ ********************************************************/
 function drawLives() {
     ctx.font = "16px Arial";
     ctx.fillStyle = "#0095DD";
@@ -79,9 +80,24 @@ function drawLine() {
     context.lineTo(500, 400);
     context.stroke();
 }
-
+function drawPlateform1() {
+    context.strokeStyle = "#202830";
+    context.lineWidth = 4;
+    context.beginPath();
+    context.moveTo(250, 350);
+    context.lineTo(330, 350);
+    context.stroke();
+}
+function drawPlateform2() {
+    context.strokeStyle = "#202830";
+    context.lineWidth = 4;
+    context.beginPath();
+    context.moveTo(50, 330);
+    context.lineTo(130, 330);
+    context.stroke();
+}
 function drawImages() {
-    context.drawImage(monsterImage, hero.x, hero.y)
+    context.drawImage(monsterImage, monster.x, monster.y)
     context.drawImage(sunImage, sun.x, sun.y);
     context.drawImage(starImage, star.x, star.y);
 }
@@ -100,59 +116,73 @@ function drawScore() {
 }
 
 function draw() {
-    requestId = undefined;
-    if (controller.up && hero.jumping == false) {
-        hero.y_velocity -= 20;
-        hero.jumping = true;
+    if (controller.up && monster.jumping == false) {
+        monster.y_velocity -= 20;
+        monster.jumping = true;
     }
     //easing motion when you add and build the velocity frame by frame
     if (controller.left) {
-        hero.x_velocity -= 0.5;
+        monster.x_velocity -= 0.5;
     }
     if (controller.right) {
-        hero.x_velocity += 0.5;
+        monster.x_velocity += 0.5;
     }
 
-    //adding 1.5 on any frame of annimation (basically until it hits the blue line)
+    //adding 1.5 on any frame of animation (basically until it hits the blue line)
     // if you were to put 0, the hero would never fall
-    hero.y_velocity += 1.5;// gravity
-    hero.x += hero.x_velocity;
-    hero.y += hero.y_velocity;
+    monster.y_velocity += 1.5;// gravity
+    monster.x += monster.x_velocity;
+    monster.y += monster.y_velocity;
 
     //reducing the x and y velocity very gradually
     // it gives you the effect that you are gradually coming to a stop
-    hero.x_velocity *= 0.9;// friction
-    hero.y_velocity *= 0.9;// friction
+    monster.x_velocity *= 0.9;// friction
+    monster.y_velocity *= 0.9;// friction
 
     // if hero is falling below floor line
     //180 = bottom of the screen - 16 la ligne, 32 le haut du hero
-    if (hero.y > 500 - 100 - 32) {
+    if (monster.y > 500 - 100 - 32) {
         //set jumping to false so we can jump again
-        hero.jumping = false;
-        hero.y = 500 - 100 - 32;
-        hero.y_velocity = 0;
+        monster.jumping = false;
+        monster.y = 500 - 100 - 32;
+        monster.y_velocity = 0;
+    }
+    //Platform1 restrictions
+    if (monster.y > 500 - 150 - 32 && monster.y < 500 - 140 - 32 && monster.x > 250 && monster.x < 330) {
+        //set jumping to false so we can jump again
+        monster.jumping = false;
+        monster.y = 500 - 150 - 32;
+        monster.y_velocity = 0;
+    }
+    //Platform2 restrictions
+    if (monster.y > 500 - 170 - 32 && monster.y < 500 - 160 - 32 && monster.x > 50 && monster.x < 130) {
+        //set jumping to false so we can jump again
+        monster.jumping = false;
+        monster.y = 500 - 170 - 32;
+        monster.y_velocity = 0;
     }
     // if hero is going off the left of the screen
-    if (hero.x < 0) {
-        hero.x = 0;
-    } else if (hero.x > 500 - 32) {// if hero goes past right boundary
+    if (monster.x < 0) {
+        monster.x = 0;
+    } else if (monster.x > 500 - 32) {// if hero goes past right boundary
         //return;
-        hero.x = 500 - 32;
+        monster.x = 500 - 32;
     }
 
     context.fillStyle = "#ffffff";
     context.fillRect(0, 0, 500, 500);// x, y, width, height
     drawLine();
+    drawPlateform1();
+    drawPlateform2();
     drawImages();
     drawScore();
     drawLives();
 
-
-    if (hero.x <= (star.x + 50) && star.x <= (hero.x + 32)
-        && hero.y <= (star.y + 50) && star.y <= (hero.y + 32)) {
+    if (monster.x <= (star.x + 50) && star.x <= (monster.x + 32)
+        && monster.y <= (star.y + 50) && star.y <= (monster.y + 32)) {
         lives--;
-        hero.x = 250;
-        hero.y = 0;
+        monster.x = 250;
+        monster.y = 0;
     } else if (!lives) {
         alert("GAME OVER");
         document.location.reload();
@@ -162,6 +192,6 @@ function draw() {
 
 };
 //adding the listeners to the specific objects
-window.addEventListener("keydown", controller.keyListener)
+window.addEventListener("keydown", controller.keyListener);
 window.addEventListener("keyup", controller.keyListener);
 window.requestAnimationFrame(draw);
